65ae8d2622d1dc1640db9747a31d6d0554aa8db4
AlonsoVS21 <viquezalonso7@gmail.com>
2023-09-29 20:21:49 -0600

    Agregar archivos de proyecto.

---
+++ Tarea2.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la cantidad de artículos comprados: ");
+        int cantidad = Convert.ToInt32(Console.ReadLine());
+
+        double precioPorProducto;
+
+        if (cantidad <= 10)
+        {
+            precioPorProducto = 20;
+        }
+        else
+        {
+            precioPorProducto = 15;
+        }
+
+        double total = cantidad * precioPorProducto;
+
+        Console.WriteLine($"Precio por producto: ${precioPorProducto}");
+        Console.WriteLine($"Total a pagar: ${total}");
+
+        Console.ReadLine(); // Esperar una entrada del usuario antes de cerrar la consola
+    }
+}
